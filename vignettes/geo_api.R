5f7de767b777eaec6629143ec1887e33bb5cdb80
mera2 <merastat@gmail.com>
2016-09-30 13:14:37 +0200

    vignettes,Description

---
+++ vignettes/geo_api.R
@@ -0,0 +1,21 @@
+## ------------------------------------------------------------------------
+
+  geo_api<-function(address){
+  
+
+  quary<-"https://maps.googleapis.com/maps/api/geocode/json"
+  address<- gsub("\\[]","\\+", address)
+  mykey="AIzaSyAkdy_DvV2xo3CkHKpVmgNfCpM7hxmsAEw"
+  totalurl<-paste(quary,"?address=",address,"&key",mykey, sep = "")
+
+
+output<-GET(url=totalurl)
+
+http_status(output)
+
+data<-content(output)
+
+return(data)
+}
+
+
